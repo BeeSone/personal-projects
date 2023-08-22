596ec86f2b290e91228ff17fb62ab46c9eed2978
BeeSone <brooks.sonet@gmail.com>
2023-08-22 13:11:39 -0500

    Created 2020AoC Day 1 Solutions
    I used a video and my own intuition to work out how Javascript's file system readFileSync worked, as well as learned some intricacies of for loops and return statements.

---
+++ AdventofCode2020/day1withfs.js
@@ -0,0 +1,17 @@
+// Plan: for each number, there should be only one corresponding number that adds to 2020,
+// so, check every line below it for the number -> if found, return both numbers
+const fs = require('fs');
+// fs.readFileSync('path') reads the contents of the entire file. .split('\n') takes the input and splits it into an array of substrings. .map(x => parseInt(x)) 
+// turns every string in that array into an int.
+const lines = fs.readFileSync('day1-input.txt', {encoding: 'utf-8'}).split('\n').filter(x => x).map(x => parseInt(x));
+
+for (let i = 0; i < lines.length; i++){
+        for (let m = i+1; m < lines.length; m++){
+            for (let j = m + 1; j < lines.length; j++){
+                if (lines[i]+lines[m]+lines[j] == 2020){
+                    return (
+                    console.log('The answer is ' + lines[i] + ', ' + lines[m] + ', and ' + lines[j]), console.log('Multiplied they are equal to ' + lines[i]*lines[m]*lines[j]));
+                }
+            }
+        }
+}
---
+++ testfolder/test.js
@@ -0,0 +1,25 @@
+// NOTE: return in a parent function ends the execution of the function immediately.
+
+// checking if this return statement can work
+const a = 3;
+const b = 5;
+function ab5(){
+    if (a + b == 8){
+        return (
+            console.log('yes'), console.log('also no'));
+    }
+} 
+
+const arrayOfNumbers = [1, 2, 3, 4]
+function testFunc(){
+    for (i = 0; i < arrayOfNumbers.length; i++){
+        for (n = i + 1; n <= arrayOfNumbers.length; n++){
+            if (parseInt(arrayOfNumbers[i]) + parseInt(arrayOfNumbers[n]) == 7){
+                return (
+                    console.log(arrayOfNumbers[i] + ' ' + arrayOfNumbers[n]));
+            }
+        } 
+    }
+}
+ab5();
+testFunc();
