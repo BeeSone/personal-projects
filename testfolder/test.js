7d093aa53de6cde2db8d20f641356234e308d5b5
BeeSone <brooks.sonet@gmail.com>
2023-09-08 21:16:51 -0500

    Took js notes

--- testfolder/test.js
+++ testfolder/test.js
@@ -1,25 +1,34 @@
 // NOTE: return in a parent function ends the execution of the function immediately.
 
 // checking if this return statement can work
-const a = 3;
-const b = 5;
-function ab5(){
-    if (a + b == 8){
-        return (
-            console.log('yes'), console.log('also no'));
-    }
-} 
+    // const a = 3;
+    // const b = 5;
+    // function ab5(){
+    //     if (a + b == 8){
+    //         return (
+    //             console.log('yes'), console.log('also no'));
+    //     }
+    // } 
 
-const arrayOfNumbers = [1, 2, 3, 4]
-function testFunc(){
-    for (i = 0; i < arrayOfNumbers.length; i++){
-        for (n = i + 1; n <= arrayOfNumbers.length; n++){
-            if (parseInt(arrayOfNumbers[i]) + parseInt(arrayOfNumbers[n]) == 7){
-                return (
-                    console.log(arrayOfNumbers[i] + ' ' + arrayOfNumbers[n]));
-            }
-        } 
-    }
-}
-ab5();
-testFunc();
+    // const arrayOfNumbers = [1, 2, 3, 4]
+    // function testFunc(){
+    //     for (i = 0; i < arrayOfNumbers.length; i++){
+    //         for (n = i + 1; n <= arrayOfNumbers.length; n++){
+    //             if (parseInt(arrayOfNumbers[i]) + parseInt(arrayOfNumbers[n]) == 7){
+    //                 return (
+    //                     console.log(arrayOfNumbers[i] + ' ' + arrayOfNumbers[n]));
+    //             }
+    //         } 
+    //     }
+    // }
+    // ab5();
+    // testFunc();
+var i = 3; 
+var p = 0; 
+// initializes function that takes an argument and adds two to it
+function adder(x) { 
+    console.log(x);
+    x = x + 2; };
+// adds two to i to create p and then refers to value of i to increment
+adder(++i); 
+console.log(i, p);
